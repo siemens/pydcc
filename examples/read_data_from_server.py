53fc1d524245369c98072f1ed9219bcc054340f3
Andreas Tobola <andreas.tobola@siemens.com>
2021-11-12 17:43:07 +0100

    Example added for reading DCC from public server

---
+++ examples/read_data_from_server.py
@@ -0,0 +1,26 @@
+# PyDCC
+#
+# Minimal working example
+#
+# Copyright (c) Siemens AG, 2021
+#
+# Authors:
+#  Andreas Tobola <andreas.tobola@siemens.com>
+#
+# This work is licensed under the terms of the MIT License.
+# See the LICENSE file in the top-level directory.
+#
+from dcc import dcc
+
+# (1) Load DCC and create the DCC object (dcco)
+dcco = dcc(url="http://127.0.0.1:8085/dcc/123") 
+
+# (2) Get UID of the DCC from DCC object
+uid = dcco.uid()
+
+# (3) Get days since last calibration from DCC object
+days_since_calibration = dcco.days_since_calibration()
+
+# (4) Print data
+print('DCC UID: %s' % uid)
+print('%d days since calibration' % days_since_calibration)
