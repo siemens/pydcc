ecc3b15e9963e121dca17427b21dc2740ef1e58f
Kai_Mienert <kai.mienert@ptb.de>
2021-12-09 01:42:16 +0100

    change expected values, DCC 3.0.0 min example

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -12,49 +12,52 @@
 # See the LICENSE file in the top-level directory.
 #
 import sys
+
 sys.path.append("../src/")
 from dcc import dcc
 import datetime
 import unittest
 
-xml_file_name = '../data/dcc/siliziumkugel_2_4_0.xml' # Example from PTB
-dcco = dcc(xml_file_name)
+xml_file_name = 'DCC_3_GrundstrukturPyDCC.xml'
+xml_file_path = '../data/dcc/' + xml_file_name
+dcco = dcc(xml_file_path)
 
 
 class TestBaseFunctions(unittest.TestCase):
 
     def test_loading_from_file(self):
-        dcc_from_file = dcc(xml_file_name) # Load DCC and crate DCC object
+        dcc_from_file = dcc(xml_file_path)  # Load DCC and crate DCC object
         self.assertTrue(dcc_from_file.is_loaded())
 
-    def test_loading_byte_array(self):        
-        with open(xml_file_name, "rb") as f:
+    def test_loading_byte_array(self):
+        with open(xml_file_path, "rb") as f:
             dcc_byte_array = f.read()
-        dcc_from_byte_array = dcc(byte_array = dcc_byte_array) # Load DCC and crate DCC object
+        dcc_from_byte_array = dcc(byte_array=dcc_byte_array)  # Load DCC and crate DCC object
         self.assertTrue(dcc_from_byte_array.is_loaded())
 
-    def test_loading_compressed_byte_array(self):        
-        with open("../data/siliziumkugel_compressed.pydcc", "rb") as f:
+    def test_loading_compressed_byte_array(self):
+        compressed_file = xml_file_name.split('.')
+        with open("../data/compressed_dcc/" + compressed_file[0] + ".pydcc", "rb") as f:
             dcc_compressed = f.read()
-        dcc_from_compresed_byte_array = dcc(compressed_dcc = dcc_compressed) # Load DCC and crate DCC object
+        dcc_from_compresed_byte_array = dcc(compressed_dcc=dcc_compressed)  # Load DCC and crate DCC object
         self.assertTrue(dcc_from_compresed_byte_array.is_loaded())
 
     def test_calibration_date(self):
-        calib_date = dcco.calibration_date()        
-        ref_date = datetime.datetime(2018, 10, 12, 0, 0)
+        calib_date = dcco.calibration_date()
+        ref_date = datetime.datetime(2021, 10, 27, 0, 0)
         self.assertEqual(calib_date, ref_date)
 
     def test_days_since_calibration(self):
         days = dcco.days_since_calibration()
-        self.assertTrue(days > 900)
+        self.assertTrue(days > 40)
 
     def test_uid(self):
         uid = dcco.uid()
-        self.assertEqual(uid, "PTB - 11129 18")
+        self.assertEqual(uid, "XXXXXXXXXXXX")
 
     def test_version(self):
         version = dcco.version()
-        self.assertEqual(version, "2.4.0")
+        self.assertEqual(version, "3.0.0")
 
     def test_uncertainty_list(self):
         uncertainty_list = dcco.uncertainty_list()
@@ -65,31 +68,31 @@ class TestBaseFunctions(unittest.TestCase):
         try:
             dcc_empty = dcc()
         except Exception:
-            exception_rised = True     
+            exception_rised = True
         self.assertTrue(exception_rised)
 
     def test_is_not_signed(self):
         self.assertFalse(dcco.is_signed())
 
     def test_is_signed(self):
-        xml_file_name = '../data/dcc/signed_siliziumkugel.xml' # Example from PTB and signed by T-Systems
-        dcc_signed = dcc(xml_file_name)        
+        xml_file_name = '../data/dcc/signed_siliziumkugel.xml'  # Example from PTB and signed by T-Systems
+        dcc_signed = dcc(xml_file_name)
         self.assertTrue(dcc_signed.is_signed())
 
     def test_compressed_dcc_crc(self):
         comp_dcc = dcco.generate_compressed_dcc()
         crc32 = comp_dcc['crc32']
-        self.assertEqual(crc32, 4022442000)
+        self.assertEqual(crc32, 251633089)
 
     def test_compressed_dcc_size(self):
         comp_dcc = dcco.generate_compressed_dcc()
         bytes_compressed = comp_dcc['bytes_compressed']
-        self.assertEqual(bytes_compressed, 5324)
+        self.assertEqual(bytes_compressed, 7826)
 
     def test_previous_report_not_available(self):
         self.assertFalse(dcco.has_previous_report())
 
- 
+
 #    def test_verify_correct_dcc_xml(self):
 #        self.assertTrue(dcco.verify_dcc_xml())
 
@@ -98,14 +101,11 @@ class TestBaseFunctions(unittest.TestCase):
 #        dcco_wrong_schema = dcc(xml_file_name_wrong_schema)        
 #        self.assertFalse(dcco_wrong_schema.verify_dcc_xml())
 
-    # Work in progress
+# Work in progress
 
-    #def test_is_signature_valid(self):
-    #    self.assertFalse(dcco.is_signature_valid())
-
+# def test_is_signature_valid(self):
+#    self.assertFalse(dcco.is_signature_valid())
 
 
 if __name__ == '__main__':
     unittest.main()
-
-
