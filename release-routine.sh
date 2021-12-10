be25da0c37bae787a94dd329d362509175e450d4
Andreas Tobola <andreas.tobola@siemens.com>
2021-12-10 09:33:38 +0100

    Copyright text added according to regulations,

--- release-routine.sh
+++ release-routine.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+# PyDCC setup script for generating PyDCC package
+#
+# Copyright (c) Siemens AG, 2021
+#
+# Authors:
+#  Andreas Tobola <andreas.tobola@siemens.com>
+#
+# This work is licensed under the terms of the MIT License.
+# See the LICENSE file in the top-level directory.
+#
 python3 increase_version.py
 pip install -e .
 
