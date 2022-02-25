aa50dd00beaca96684283c747faf00e29439ca77
Andreas Tobola <andreas.tobola@siemens.com>
2022-02-25 04:22:11 +0100

    Directory merged from master

---
+++ dcc/__init__.py
@@ -0,0 +1,13 @@
+# PyDCC
+#
+# Init file
+#
+# Copyright (c) Siemens AG, 2021
+#
+# Authors:
+#  Andreas Tobola <andreas.tobola@siemens.com>
+#
+# This work is licensed under the terms of the MIT License.
+# See the LICENSE file in the top-level directory.
+#
+from .dcc import DCC, dcc
