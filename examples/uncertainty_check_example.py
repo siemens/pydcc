b931e7aefb22735388a542cacedb05db774db554
Andreas Tobola <andreas.tobola@siemens.com>
2022-07-08 11:53:58 +0200

    Uncertainty verification example finished.

--- examples/uncertainty_check_example.py
+++ examples/uncertainty_check_example.py
@@ -18,23 +18,59 @@
 import sys
 sys.path.append("../dcc/")
 from dcc import DCC
+import numpy as np
 
 # Load DCC and create the DCC object (dcco)
-dcco = DCC('../data/Uncertainty4_PyDCC.xml')
+dcco = DCC('../data/dcc/dcc_gp_temperature_typical_v12.xml')
 
-print("mandatory language")
-lang = dcco.mandatoryLang()
-print(lang)
-
-
-print("all results")
-res = dcco.get_calibration_results('de')
-for i in res:
-   print(i)
+#lang = dcco.mandatoryLang()
 
 
 
 
+def calibration_results_pre_processor(cres):
+
+    # Convert calibration result value list from string separated by blanks to lsit of floating point values
+    string_list = cres[0].split(' ')
+    float_list = []
+    for item in string_list:
+        float_list.append(float(item))
+    cres[0] = float_list
+
+    # Convert uncertainty to floating pint values
+    cres[3] = float(cres[3])
+
+    return cres
+
+def search_calibration_results(cres, searched_name):
+    for i in cres:
+        name = i[0]
+        if name.find(searched_name) > -1:
+            selected_calib_result = i[1]
+            pre_processed_calib_result = calibration_results_pre_processor(selected_calib_result)
+            return pre_processed_calib_result
 
 
+def print_results(cres):
+   for i in cres:
+      print(i)
 
+
+cres = dcco.get_calibration_results('en')
+#print_results(cres)
+
+mres = search_calibration_results(cres, 'Measurement error')
+print(mres)
+
+# Verification of results
+uncertainty95_requirement = 0.071
+uncertainty95_actual = mres[3]
+
+print("Uncertainty requirement is a maximum of %.3f" % uncertainty95_requirement)
+print("Actual uncertainty is %.3f" % uncertainty95_actual)
+device_mmets_requirements = uncertainty95_actual <= uncertainty95_requirement 
+print('Device meets requirements:', device_mmets_requirements)
+
+
+uncertainty_list = mres[0]
+print(np.std(uncertainty_list) * 2)
