8cf812ddfe8f7e7af1e53341ab27dd3b6416d63b
Andreas Tobola <andreas.tobola@siemens.com>
2023-03-20 15:55:54 +0100

    Adapted example after moving functionality to module.

--- examples/read_identification.py
+++ examples/read_identification.py
@@ -21,52 +21,24 @@ dcco = DCC(xml_file_name) # Load DCC and crate DCC object
 
 print(dcco.item_id())
 
+def print_serial_number(serial_number):
+	if serial_number is not None:
+		print("Serial number: " + serial_number)
+	else:
+		print("Serial number not found.")
 
-def get_item_id_by_name(dcco, searched_name, searched_language = None, searched_issuer = None, ):
-	id_list = dcco.item_id()['identifications']['identification']
-	for id in id_list:
-		names = id['name']['content']
-		issuer = id['issuer']
-		if searched_issuer is not None:
-			if not issuer == searched_issuer:
-				continue
-		for name in names:
-			language = name['@lang']
-			if searched_language is not None:
-				if not language == searched_language:
-					continue
-			name_text =  name['#text']
-			if name_text == searched_name:
-				value = id['value']
-				return value
-	return None
+# A search string can be specified. The first match will be returned.
+serial_number = dcco.get_item_id_by_name('Serial no.')
+print_serial_number(serial_number)
 
+serial_number = dcco.get_item_id_by_name('Serial no.', 'en')
+print_serial_number(serial_number)
 
-serial_number = get_item_id_by_name(dcco, 'Serial no.')
+serial_number = dcco.get_item_id_by_name('Serial no.', 'en', 'manufacturer')
+print_serial_number(serial_number)
 
-if serial_number is not None:
-	print("Serial number: " + serial_number)
-else:
-	print("Serial number not found.")
+serial_number = dcco.get_item_id_by_name('Serien Nr.')
+print_serial_number(serial_number)
 
-
-serial_number = get_item_id_by_name(dcco, 'Serial no.', 'en')
-
-if serial_number is not None:
-	print("Serial number: " + serial_number)
-else:
-	print("Serial number not found.")
-
-
-serial_number = get_item_id_by_name(dcco, 'Serial no.', 'en', 'manufacturer')
-
-if serial_number is not None:
-	print("Serial number: " + serial_number)
-else:
-	print("Serial number not found.")
-
-
-
-#print(dcco.item_id()['identifications']['identification'])
-#for elem in dcco.item_id()['identifications']['identification']:
-#	print(elem)
+serial_number = dcco.get_item_id_by_name('Seriennummer')
+print_serial_number(serial_number)
