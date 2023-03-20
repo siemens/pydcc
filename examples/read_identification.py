04ad414811819b9bd09892f40b2f6c8f499890a5
Andreas Tobola <andreas.tobola@siemens.com>
2023-03-20 14:09:52 +0100

    Extended item id example.

--- examples/read_identification.py
+++ examples/read_identification.py
@@ -18,9 +18,55 @@ from dcc import DCC
 xml_file_name = '../data/dcc/dcc_gp_temperature_typical_v12.xml'
 dcco = DCC(xml_file_name) # Load DCC and crate DCC object
 
-print('----ID---')
+
 print(dcco.item_id())
 
+
+def get_item_id_by_name(dcco, searched_name, searched_language = None, searched_issuer = None, ):
+	id_list = dcco.item_id()['identifications']['identification']
+	for id in id_list:
+		names = id['name']['content']
+		issuer = id['issuer']
+		if searched_issuer is not None:
+			if not issuer == searched_issuer:
+				continue
+		for name in names:
+			language = name['@lang']
+			if searched_language is not None:
+				if not language == searched_language:
+					continue
+			name_text =  name['#text']
+			if name_text == searched_name:
+				value = id['value']
+				return value
+	return None
+
+
+serial_number = get_item_id_by_name(dcco, 'Serial no.')
+
+if serial_number is not None:
+	print("Serial number: " + serial_number)
+else:
+	print("Serial number not found.")
+
+
+serial_number = get_item_id_by_name(dcco, 'Serial no.', 'en')
+
+if serial_number is not None:
+	print("Serial number: " + serial_number)
+else:
+	print("Serial number not found.")
+
+
+serial_number = get_item_id_by_name(dcco, 'Serial no.', 'en', 'manufacturer')
+
+if serial_number is not None:
+	print("Serial number: " + serial_number)
+else:
+	print("Serial number not found.")
+
+
+
 #print(dcco.item_id()['identifications']['identification'])
-for elem in dcco.item_id()['identifications']['identification']:
-	print(elem)
+#for elem in dcco.item_id()['identifications']['identification']:
+#	print(elem)
